c3a10ffeed94d802b134889cf246f3c22ac47246
Apebble38 <clawrenc@oswego.edu>
2016-12-07 22:12:41 -0500

    Created account database

---
+++ app/src/main/java/com/example/mikey/maps/TrailHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.mikey.maps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrailHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trail_history);
+    }
+}
